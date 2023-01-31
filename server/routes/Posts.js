40ea67d01e1bd2bf39e85e613142624c1432b7d9
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2023-01-30 22:39:46 -0300

    Posting, jsons and requests
    Requests have been configured and tested.

--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello World!")
+const { Posts } = require("../models");
+
+router.get("/", async (req, res) => {
+  const listOfPosts = await Posts.findAll();
+  res.json(listOfPosts);
 });
 
+router.post("/", async (req, res) => {
+  const post = req.body;
+  await Posts.create(post);
+  res.json(post);
+});
 
 module.exports = router;
