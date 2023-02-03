032e0eb3cb8cfb71c4eebca5cf0cc0ff1e0cee59
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2023-02-02 21:47:10 -0300

    Added validation and initial values
    Added validation, inititial values.
    Also added error messages for when the input values don't meet the set requirements.

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,13 +1,36 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-
+import * as Yup from "yup";
+import axios from "axios";
 
 function CreatePost() {
+  const initialValues = {
+    title: "",
+    postText: "",
+    username: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("You must input a Title!"),
+    postText: Yup.string().required(),
+    username: Yup.string().min(3).max(15).required(),
+  });
+
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/posts", data).then((response) => {
+      console.log("IT WORKED!");
+    })
+  };
   return (
     <div className="createPostPage">
-      <Formik>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
         <Form className="formContainer">
           <label>Title: </label>
+          <ErrorMessage component="span" name="title" />
           <Field
             autocomplete="off"
             id="inputCreatePost"
@@ -15,6 +38,7 @@ function CreatePost() {
             placeholder="(Ex. Title...)"
           />
           <label>Post: </label>
+          <ErrorMessage component="span" name="postText"/>
           <Field
             autocomplete="off"
             id="inputCreatePost"
@@ -22,6 +46,7 @@ function CreatePost() {
             placeholder="(Ex. Post...)"
           />
           <label>Username: </label>
+          <ErrorMessage component="span" name="username"/>
           <Field
             autocomplete="off"
             id="inputCreatePost"
