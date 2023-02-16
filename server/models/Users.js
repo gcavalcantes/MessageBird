c95136e33d5ef26ae044bc5fc35e4574c334827c
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2023-02-16 17:28:55 -0300

    Liking a post
    Posts can now be liked in the backend.
    Tested.

--- server/models/Users.js
+++ server/models/Users.js
@@ -10,10 +10,10 @@ module.exports = (sequelize, DataTypes) => {
     },
   });
 
-  /* Users.associate = (models) => {
-    Users.hasMany(models.Comments, {
+  Users.associate = (models) => {
+    Users.hasMany(models.Likes, {
       onDelete: "cascade",
-    });
-  }; */
+    })
+  }
   return Users;
 };
